feef3328d08387424877c4fe8d74055ca5c6521f
Nayuki Minase <nayuki@eigenstate.org>
2009-07-01 17:52:45 -0400

    Changed display names of registers from AT&T syntax to Intel syntax.

--- src/org/p79068/assembler/operand/Register16.java
+++ src/org/p79068/assembler/operand/Register16.java
@@ -7,28 +7,28 @@ package org.p79068.assembler.operand;
 public final class Register16 extends Register {
 	
 	/** The AX register. */
-	public static Register16 AX_REGISTER = new Register16("%ax", 0);
+	public static Register16 AX_REGISTER = new Register16("ax", 0);
 	
 	/** The CX register. */
-	public static Register16 CX_REGISTER = new Register16("%cx", 1);
+	public static Register16 CX_REGISTER = new Register16("cx", 1);
 	
 	/** The DX register. */
-	public static Register16 DX_REGISTER = new Register16("%dx", 2);
+	public static Register16 DX_REGISTER = new Register16("dx", 2);
 	
 	/** The BX register. */
-	public static Register16 BX_REGISTER = new Register16("%bx", 3);
+	public static Register16 BX_REGISTER = new Register16("bx", 3);
 	
 	/** The SP register. */
-	public static Register16 SP_REGISTER = new Register16("%sp", 4);
+	public static Register16 SP_REGISTER = new Register16("sp", 4);
 	
 	/** The BP register. */
-	public static Register16 BP_REGISTER = new Register16("%bp", 5);
+	public static Register16 BP_REGISTER = new Register16("bp", 5);
 	
 	/** The SI register. */
-	public static Register16 SI_REGISTER = new Register16("%si", 6);
+	public static Register16 SI_REGISTER = new Register16("si", 6);
 	
 	/** The DI register. */
-	public static Register16 DI_REGISTER = new Register16("%di", 7);
+	public static Register16 DI_REGISTER = new Register16("di", 7);
 	
 	
 	
--- src/org/p79068/assembler/operand/SegmentRegister.java
+++ src/org/p79068/assembler/operand/SegmentRegister.java
@@ -7,22 +7,22 @@ package org.p79068.assembler.operand;
 public final class SegmentRegister extends Register {
 	
 	/** The ES register. */
-	public static SegmentRegister ES_REGISTER = new SegmentRegister("%es", 0);
+	public static SegmentRegister ES_REGISTER = new SegmentRegister("es", 0);
 	
 	/** The CS register. */
-	public static SegmentRegister CS_REGISTER = new SegmentRegister("%cs", 1);
+	public static SegmentRegister CS_REGISTER = new SegmentRegister("cs", 1);
 	
 	/** The SS register. */
-	public static SegmentRegister SS_REGISTER = new SegmentRegister("%ss", 2);
+	public static SegmentRegister SS_REGISTER = new SegmentRegister("ss", 2);
 	
 	/** The DS register. */
-	public static SegmentRegister DS_REGISTER = new SegmentRegister("%ds", 3);
+	public static SegmentRegister DS_REGISTER = new SegmentRegister("ds", 3);
 	
 	/** The FS register. */
-	public static SegmentRegister FS_REGISTER = new SegmentRegister("%fs", 4);
+	public static SegmentRegister FS_REGISTER = new SegmentRegister("fs", 4);
 	
 	/** The GS register. */
-	public static SegmentRegister GS_REGISTER = new SegmentRegister("%gs", 5);
+	public static SegmentRegister GS_REGISTER = new SegmentRegister("gs", 5);
 	
 	
 	
