346c2b10d09e7667271d87b75812d3c599c9a1c8
Nayuki Minase <nayuki@eigenstate.org>
2009-07-01 17:38:42 -0400

    Removed vestigial operand parser.

--- src/org/p79068/assembler/operand/Operand.java
+++ src/org/p79068/assembler/operand/Operand.java
@@ -7,17 +7,4 @@ package org.p79068.assembler.operand;
  * @see Register
  * @see Memory32
  */
-public class Operand {
-	
-	public static Operand parseOperand(String op) {
-		Operand result = null;
-		
-		if (result == null) try { result = new ImmediateValue(Integer.parseInt(op)); } catch (NumberFormatException e) {}
-		
-		if (result == null)
-			throw new IllegalArgumentException("Illegal operand: " + op);
-		
-		return result;
-	}
-	
-}
+public class Operand {}
