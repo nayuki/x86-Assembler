9f85658577daafbdb8e100a791c2a5782f48cea5
Nayuki Minase <nayuki@eigenstate.org>
2009-07-02 00:26:35 -0400

    Added command line argument check.

--- src/org/p79068/assembler/Main.java
+++ src/org/p79068/assembler/Main.java
@@ -10,6 +10,11 @@ import org.p79068.assembler.parser.Parser;
 public final class Main {
 	
 	public static void main(String[] args) throws IOException {
+		if (args.length != 2) {
+			System.err.println("Usage: java org.p79068.assembler.Main INPUTFILE OUTPUTFILE");
+			System.exit(1);
+		}
+		
 		File inputfile = new File(args[0]);
 		File outputfile = new File(args[1]);
 		
