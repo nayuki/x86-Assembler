3f9d0072e2ab1427d9059929a88633bb5a7c28a6
Nayuki Minase <nayuki@eigenstate.org>
2009-07-03 01:34:36 -0400

    Changed output format from raw to ELF. Oh my God, this assembler can produce real executable programs now!

--- src/org/p79068/assembler/generator/Assembler.java
+++ src/org/p79068/assembler/generator/Assembler.java
@@ -1,6 +1,7 @@
 package org.p79068.assembler.generator;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -28,7 +29,7 @@ public final class Assembler {
 	
 	
 	private static void computeLabelOffsets(Program program) {
-		int offset = 0;
+		int offset = 0x08000054;
 		for (Statement st : program.getStatements()) {
 			if (st instanceof InstructionStatement) {
 				InstructionStatement ist = (InstructionStatement)st;
@@ -45,11 +46,100 @@ public final class Assembler {
 	
 	
 	private static void assembleProgram(Program program, File outputfile) throws IOException {
+		byte[] code = assembleToBytes(program);
 		OutputStream out0 = new FileOutputStream(outputfile);
 		OutputStream out = new BufferedOutputStream(out0);
 		
 		try {
-			int offset = 0;
+			writeElfHeader(code, out);
+			out.write(code);
+		} finally {
+			out.close();
+		}
+	}
+	
+	
+	private static void writeElfHeader(byte[] code, OutputStream out) throws IOException {
+		// Write ELF header
+		
+		// e_ident
+		out.write(new byte[]{0x7F, 'E', 'L', 'F'});  // Magic number
+		out.write(0x01);  // Class: 0x01 = 32-bit
+		out.write(0x01);  // Data: 0x01 = Little endian
+		out.write(0x01);  // Version: 0x01 = Current
+		out.write(new byte[9]);  // Padding: 9 bytes of zeros
+		
+		// e_type
+		out.write(0x02); out.write(0x00);  // Type: 0x0002 = Executable file
+		
+		// e_machine
+		out.write(0x03); out.write(0x00);  // Machine: 0x0003 = Intel 80386
+		
+		// e_version
+		out.write(0x01); out.write(0x00); out.write(0x00); out.write(0x00);  // Version: 0x00000001 = Current
+		
+		// e_entry
+		out.write(0x54); out.write(0x00); out.write(0x00); out.write(0x08);  // Entry point: 0x08000000
+		
+		// e_phoff
+		out.write(0x34); out.write(0x00); out.write(0x00); out.write(0x00);
+		
+		// e_shoff
+		out.write(0x00); out.write(0x00); out.write(0x00); out.write(0x00);
+		
+		// e_flags
+		out.write(0x00); out.write(0x00); out.write(0x00); out.write(0x00);
+		
+		// e_ehsize
+		out.write(0x34); out.write(0x00);
+		
+		// e_phentsize
+		out.write(0x20); out.write(0x00);
+		
+		// e_phnum
+		out.write(0x01); out.write(0x00);
+		
+		// e_shentsize
+		out.write(0x00); out.write(0x00);
+		
+		// e_shnum
+		out.write(0x00); out.write(0x00);
+		
+		// e_shstrndx
+		out.write(0x00); out.write(0x00);
+		
+		// Write program header
+		
+		// p_type
+		out.write(0x01); out.write(0x00); out.write(0x00); out.write(0x00);  // Type: 0x00000001 = Loadable
+		
+		// p_offset
+		out.write(0x54); out.write(0x00); out.write(0x00); out.write(0x00);
+		
+		// p_vaddr
+		out.write(0x54); out.write(0x00); out.write(0x00); out.write(0x08);
+		
+		// p_paddr
+		out.write(0x00); out.write(0x00); out.write(0x00); out.write(0x00);  // Physical address: Ignored
+		
+		// p_filesz
+		out.write(code.length >>> 0); out.write(code.length >>> 8); out.write(code.length >>> 16); out.write(code.length >>> 24);
+		
+		// p_memsz
+		out.write(code.length >>> 0); out.write(code.length >>> 8); out.write(code.length >>> 16); out.write(code.length >>> 24);
+		
+		// p_flags
+		out.write(0x07); out.write(0x00); out.write(0x00); out.write(0x00);
+		
+		// p_align
+		out.write(0x00); out.write(0x10); out.write(0x00); out.write(0x00);
+	}
+	
+	
+	private static byte[] assembleToBytes(Program program) {
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream(); 
+			int offset = 0x08000054;
 			for (Statement st : program.getStatements()) {
 				if (st instanceof InstructionStatement) {
 					InstructionStatement ist = (InstructionStatement)st;
@@ -64,8 +154,9 @@ public final class Assembler {
 						throw new AssertionError("Label offset mismatch");
 				}
 			}
-		} finally {
-			out.close();
+			return out.toByteArray();
+		} catch (IOException e) {
+			throw new AssertionError(e);
 		}
 	}
 	
